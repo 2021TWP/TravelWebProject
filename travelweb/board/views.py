025fad838261b46d7c11cc450dddd186cd81d273
chaesuhyeon <ddd8177@gmail.com>
2021-10-22 11:14:18 +0900

    FEAT:Board update view

--- travelweb/board/views.py
+++ travelweb/board/views.py
@@ -79,17 +79,17 @@ def board_update(request, pk):
         return Response(serializer.data)
 
 
-@api_view(['PUT'])
-def board_update(request, pk):
-    board = Board.objects.get(id=pk)
-    serializer = BoardSerializer(instance=board, data=request.data)
-    print(request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "Updated!"})
-    else:
-        # return Response({"message": "Failed to update!"})
-        return Response(serializer.data)
+# @api_view(['PUT'])
+# def board_update(request, pk):
+#     board = Board.objects.get(id=pk)
+#     serializer = BoardSerializer(instance=board, data=request.data)
+#     print(request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response({"message": "Updated!"})
+#     else:
+#         # return Response({"message": "Failed to update!"})
+#         return Response(serializer.data)
 
 
 @api_view(['DELETE'])
