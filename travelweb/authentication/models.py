fda6d5f736c9cdb965196c8ccc44e6a58f0ad936
kcho32 <kwdc0302@gmail.com>
2021-10-26 14:53:26 +0900

    FIX: Minor Changes for service improvement

--- travelweb/authentication/models.py
+++ travelweb/authentication/models.py
@@ -6,9 +6,9 @@ from schedule.models import Schedule
 
 class UserGroup(models.Model):
     group_name = models.CharField(max_length=20, blank=False, unique=True)
-    pin = models.CharField(max_length=8, blank=False)
+    pin = models.CharField(max_length=8, blank=False, null=False)
     created_date = models.DateTimeField(blank=True, null=True)
-    schedules = models.ManyToManyField(Schedule, blank=True, null=True)
+    schedules = models.ManyToManyField(Schedule, blank=True)
 
     def __str__(self):
         return self.group_name
@@ -17,7 +17,7 @@ class UserGroup(models.Model):
 class UserInfo(AbstractUser):
     name = models.CharField(max_length=20, blank=False)
     email = models.EmailField(unique=True, blank=False)
-    g_id = models.ManyToManyField(UserGroup, blank=True, null=True)
+    g_id = models.ManyToManyField(UserGroup, blank=True)
 
     # like_boards = models.ManyToManyField('Board', blank=True, null=True, related_name='like_users')
 
