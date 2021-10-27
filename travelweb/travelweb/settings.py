d8e83832c96f750072c48c334e42a9c546eeb4ea
MinSeo Cho <chj2171@gmail.com>
2021-10-27 11:01:05 +0900

    FEAT : Fixed setting.py

--- travelweb/travelweb/settings.py
+++ travelweb/travelweb/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'travelweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'travel',
-#         'USER': 'root',
-#         'PASSWORD': '1234',
-#         'HOST': '184.169.172.41',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'travel',
+        'USER': 'remote',
+        'PASSWORD': 'remotepwd123',
+        'HOST': '184.169.172.41',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password dvalidation
